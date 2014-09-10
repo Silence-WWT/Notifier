594f5d06433f1e7596668a56498de4f3c63824ce
silencewwt <silencewwt@gmail.com>
2014-09-11 00:23:04 +0800

    增加archive与grade侧边栏

--- NFBasic/views.py
+++ NFBasic/views.py
@@ -1,17 +1,81 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ObjectDoesNotExist
 from NFBasic.forms import *
 from NFBasic.models import *
+from datetime import datetime
 
 
 def index(request):
-    return render_to_response('index.html', {'request': request})
+    recent_list = Notification.objects.all().order_by('modified_time').reverse()[:10]
+    return render_to_response('index.html', {'request': request, 'recent_list': recent_list})
 
 
-def detail(request):
-    pass
+def detail(request, pid):
+    notification = get_object_or_404(Notification, pk=pid)
+    return render_to_response('detail.html', {'request': request, 'notification': notification})
+
+
+def view(request, page=''):
+    nf_list = list(Notification.objects.all().order_by('modified_time').reverse())
+    return pageinate(request, nf_list, page)
+
+
+def get_archive(request, time, page=''):
+    archive_time = datetime(int(time[:4]), int(time[5:]), 1)
+    archive_month = get_object_or_404(NotificationMonth, month=archive_time)
+    nf_list = list(archive_month.notification_set.all().order_by('modified_time').reverse())
+    if not nf_list:
+        return Http404
+    archive_month = archive_month.month.strftime('%Y-%m')
+    return pageinate(request, nf_list, page, archive_month)
+
+
+def get_archive_dict_list():
+    archive_list = list(NotificationMonth.objects.all().order_by('month').reverse())
+    archive_dict_list = []
+    for month in archive_list:
+        archive_dict_list.append({'month': month.month.strftime('%Y-%m'), 'num': month.notification_set.count()})
+    return archive_dict_list
+
+
+def get_grade(request, grade, page=''):
+    grade = get_object_or_404(Grade, grade=grade)
+    nf_list = list(grade.notification_set.all())
+    if not nf_list:
+        return Http404
+    return pageinate(request, nf_list, page, grade=grade)
+
+
+def get_grade_dict_list():
+    grade_list = list(Grade.objects.all())
+    grade_dict_list = []
+    for grade in grade_list:
+        if grade.notification_set.count():
+            grade_dict_list.append({'grade': grade.grade, 'num': grade.notification_set.count()})
+    return grade_dict_list
+
+
+def pageinate(request, nf_list, page, archive_month=None, grade=None):
+    try:
+        page = int(page)
+    except TypeError:
+        page = 1
+    pages = int((len(nf_list) + 10 - 1) / 10)
+    if page > pages or page < 1:
+        return HttpResponseRedirect('/views/')
+    context = {'nf_list': nf_list, 'pages': pages,
+               'archive_dict_list': get_archive_dict_list(), 'archive_month': archive_month,
+               'grade_dict_list': get_grade_dict_list(), 'grade': grade}
+    if pages > 1:
+        context['nf_list'] = nf_list[(page-1)*10: page*10]
+        context['page'] = page
+        context['last_page'] = page - 1
+        context['next_page'] = page + 1
+        context['range'] = range(1, pages + 1)
+    return render_to_response('notifications.html', context)
 
 
 @csrf_exempt
@@ -29,8 +93,16 @@ def notify(request):
                     grade = Grade.objects.create(grade=nf_data['grade'])
                 else:
                     grade = grade[0]
+                created_time = datetime.now()
+                created_time = datetime(created_time.year, created_time.month, 1)
+                nf_month = NotificationMonth.objects.filter(month__exact=created_time)
+                if not nf_month:
+                    month = NotificationMonth.objects.create(month=created_time)
+                else:
+                    month = nf_month[0]
                 user = request.user
-                notification = Notification.objects.create(title=title, content=content, user=user, grade=grade)
+                notification = Notification.objects.create(title=title, content=content, user=user, grade=grade,
+                                                           month=month)
                 if file_form.is_valid():
                     notification.file = request.FILES['file']
                     notification.save()
--- Notifier/urls.py
+++ Notifier/urls.py
@@ -16,7 +16,10 @@ urlpatterns = patterns('',
     url(r'^login/$', views.user_login, name='user_login'),
     url(r'^logout/$', views.user_logout, name='user_logout'),
 
-    url(r'^detail/$', views.detail, name='detail'),
+    url(r'^views/(?:(\d+)/)?$', views.view, name='view'),
+    url(r'^archive/(\d{4}-\d{2})/(?:(\d+)/)?$', views.get_archive, name='archive'),
+    url(r'^grade/(\w+?)/(?:(\d+)/)?$', views.get_grade, name='grade'),
+    url(r'^detail/(\d+)/$', views.detail, name='detail'),
     url(r'^notify/$', views.notify, name='notify'),
 
 )
