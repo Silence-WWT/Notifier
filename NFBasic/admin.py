d8e4a8a3ba7be186244b06fcfcef0c5432da6b89
silencewwt <silencewwt@gmail.com>
2014-09-23 19:04:22 +0800

    添加后台admin界面

--- NFBasic/admin.py
+++ NFBasic/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from NFBasic.models import *
 
-# Register your models here.
+
+admin.site.register(Notification)
+admin.site.register(Grade)
+admin.site.register(NotificationMonth)
